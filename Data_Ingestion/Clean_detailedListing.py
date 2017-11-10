e036cd1799d2a54c0524bcf2a16caf4be62907d9
QIMING WANG <qimingwang333@gmail.com>
2017-11-10 12:14:19 -0800

    Add comment, edit the path

--- Data_Ingestion/Clean_detailedListing.py
+++ Data_Ingestion/Clean_detailedListing.py
@@ -1,28 +1,33 @@
+# Clean the detailed list csv file for 6 cities chosen
+# You need to modify the read_csv path and to_csv path for your local
+# If you add cities more than: Boston, LA, NYC, SF, SEA, Chicago, you need to check the fileds to
+# make sure the field of the city you choose include in the scheme_dict 
+
 import pandas as pd
 
 try:
-	c1 = pd.read_csv('/Users/qiming/Desktop/AirbnbProject/dataset/detailed_listing/listings_BST.csv',low_memory=False)
-	c2 = pd.read_csv('/Users/qiming/Desktop/AirbnbProject/dataset/detailed_listing/listings_LA.csv',low_memory=False)
-	c3 = pd.read_csv('/Users/qiming/Desktop/AirbnbProject/dataset/detailed_listing/listings_NYC.csv',low_memory=False)
-	c4 = pd.read_csv('/Users/qiming/Desktop/AirbnbProject/dataset/detailed_listing/listings_SEA.csv',low_memory=False)
-	c5 = pd.read_csv('/Users/qiming/Desktop/AirbnbProject/dataset/detailed_listing/listings_CHI.csv',low_memory=False)
-	c6 = pd.read_csv('/Users/qiming/Desktop/AirbnbProject/dataset/detailed_listing/listings_SF.csv',low_memory=False)
+	c1 = pd.read_csv('your_detailed_listing_csv_file_path',low_memory=False)
+	c2 = pd.read_csv('your_detailed_listing_csv_file_path',low_memory=False)
+	c3 = pd.read_csv('your_detailed_listing_csv_file_path',low_memory=False)
+	c4 = pd.read_csv('your_detailed_listing_csv_file_path',low_memory=False)
+	c5 = pd.read_csv('your_detailed_listing_csv_file_path',low_memory=False)
+	c6 = pd.read_csv('your_detailed_listing_csv_file_path',low_memory=False)
 
 except IOError:
         print("Wrong file or file path")
 
 
-scheme_dict = set([ 'id', 'listing_url',	'last_scraped',	'name',	'transit',	'host_id',	'host_url',
-					'host_name', 'host_response_rate', 'host_acceptance_rate', 'neighbourhood', 
-					'neighbourhood_cleansed', 'neighbourhood_group_cleansed',	'city',	'state', 'zipcode', 
-					'country_code', 'country',	'latitude', 'longitude', 'is_location_exact', 
-					'property_type', 'room_type', 'accommodates', 'bathrooms', 'bedrooms',
-					'beds',	'bed_type', 'square_feet', 'price', 'weekly_price', 'monthly_price', 
-					'security_deposit', 'cleaning_fee', 'minimum_nights','maximum_nights',	
-					'has_availability',	'calendar_last_scraped','number_of_reviews', 'first_review',	
-					'last_review',	'review_scores_rating',	'review_scores_accuracy', 
-					'review_scores_cleanliness', 'review_scores_checkin', 'review_scores_communication', 
-					'review_scores_location','review_scores_value','cancellation_policy','reviews_per_month'])
+scheme_dict = set([ 'id', 'listing_url','last_scraped',	'name',	'transit',	'host_id',	'host_url',
+		    'host_name', 'host_response_rate', 'host_acceptance_rate', 'neighbourhood', 
+		    'neighbourhood_cleansed', 'neighbourhood_group_cleansed',	'city',	'state', 'zipcode', 
+		    'country_code', 'country',	'latitude', 'longitude', 'is_location_exact', 
+		    'property_type', 'room_type', 'accommodates', 'bathrooms', 'bedrooms',
+		    'beds', 'bed_type', 'square_feet', 'price', 'weekly_price', 'monthly_price', 
+		    'security_deposit', 'cleaning_fee', 'minimum_nights','maximum_nights',	
+		    'has_availability',	'calendar_last_scraped','number_of_reviews', 'first_review',	
+		    'last_review', 'review_scores_rating', 'review_scores_accuracy', 
+		    'review_scores_cleanliness', 'review_scores_checkin', 'review_scores_communication', 
+		    'review_scores_location','review_scores_value','cancellation_policy','reviews_per_month'])
 
 def clean_col(df):
 	col_name = df.columns.tolist()
@@ -40,6 +45,6 @@ if __name__ == '__main__':
 	seattle = clean_col(c6)
 	combined = pd.concat([boston, chicago, los_angeles, new_york, san_fran, seattle])
 	try:
-		combined.to_csv('/Users/qiming/Desktop/detailed_listing.csv',index=False,header=False)
+		combined.to_csv('your_output_csv_file_path',index=False,header=False)
 	except IOError:
 		print("Wrong path for wirting file")
